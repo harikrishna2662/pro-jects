17bfe3e9b1dc63a57290fdeb31c4d583f4151f83
Hari <hari2662.hk@gmail.com>
2021-11-24 16:49:39 +0530

    first commit

---
+++ ForLoopEx.java
@@ -0,0 +1,11 @@
+class ForLoopEx
+{
+	public static void main(String args[])
+	{
+		for(int i=0;i<10;i++)
+		{
+			System.out.println(i+" I Love You");
+		}
+		
+	}
+}
---
+++ SwitchEx.java
@@ -0,0 +1,45 @@
+import java.util.*;
+class SwitchEx{
+		public static void main(String ar[]){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Pleae enter your fav Number: ");
+		int a = sc.nextInt();
+		switch(a)
+		{
+		case 1 : 
+		System.out.println("your fav number is "+a);
+		break;
+		case 2 : 
+		System.out.println("your fav number is "+a);
+		break;
+		case 3 : 
+		System.out.println("your fav number is "+a);
+		break;
+		case 4 : 
+		System.out.println("your fav number is "+a);
+		break;
+		case 5 : 
+		System.out.println("your fav number is "+a);
+		break;
+		case 6 : 
+		System.out.println("your fav number is "+a);
+		break;
+		case 7 : 
+		System.out.println("your fav number is "+a);
+		break;
+		case 8 : 
+		System.out.println("your fav number is "+a);
+		break;
+		case 9 : 
+		System.out.println("your fav number is "+a);
+		break;
+		default : 
+		System.out.println("Please enter number between 0 to 9........ ");
+		}
+		
+		
+		}
+
+
+
+}
